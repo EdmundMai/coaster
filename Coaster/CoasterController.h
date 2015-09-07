607f76d0081efa63f526f2cec57388bf15b67cf3
Edmund Mai <edmundm@crowdtap.com>
2015-09-07 00:04:15 -0400

    adds google banner ads

--- Coaster/CoasterController.h
+++ Coaster/CoasterController.h
@@ -7,8 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <iAd/iAd.h>
 
-@interface CoasterController : UIViewController <ADBannerViewDelegate>
+@interface CoasterController : UIViewController
 
 @end
--- Coaster/CoasterController.m
+++ Coaster/CoasterController.m
@@ -7,15 +7,19 @@
 //
 
 #import "CoasterController.h"
+#import <iAd/iAd.h>
+@import GoogleMobileAds;
 
-@interface CoasterController ()
+@interface CoasterController ()  <ADBannerViewDelegate, GADBannerViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *coasterImage;
-@property (assign, nonatomic) BOOL bannerIsVisible;
-@property (strong, nonatomic) ADBannerView *adBanner;
-@property (weak, nonatomic) IBOutlet UIButton *settingsButton;
-@property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
 @property (assign, nonatomic) NSInteger imageIndex;
+
+@property (weak, nonatomic) IBOutlet UIButton *settingsButton;
+@property (weak, nonatomic) IBOutlet UIView *instructionsContainer;
+
+@property (strong, nonatomic) ADBannerView *adBanner;
+@property (strong, nonatomic) GADBannerView *gadBanner;
 
 @end
 
@@ -23,9 +27,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.instructionsLabel.hidden = YES;
-//    self.instructionsLabel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"messagebubble"]];
-    
+
+    self.instructionsContainer.hidden = YES;
+
     UITapGestureRecognizer *screenTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(screenTapped)];
     [self.view addGestureRecognizer:screenTapRecognizer];
     
@@ -39,8 +43,8 @@
     imageSwipeRightRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
     [self.coasterImage addGestureRecognizer:imageSwipeRightRecognizer];
     
-    self.adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 50, 320, 50)];
-    self.adBanner.delegate = self;
+    [self initIadBanner];
+    [self initGadBanner];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -49,50 +53,43 @@
 }
 
 - (IBAction)settingsButtonClicked:(id)sender {
-    if (self.instructionsLabel.hidden) {
-        self.instructionsLabel.hidden = NO;
+    if (self.instructionsContainer.hidden) {
+        self.instructionsContainer.hidden = NO;
     } else {
-        self.instructionsLabel.hidden = YES;
+        self.instructionsContainer.hidden = YES;
     }
 }
 
 - (void) screenTapped {
-    self.instructionsLabel.hidden = YES;
+    self.instructionsContainer.hidden = YES;
 }
 
 - (NSArray *)images {
-    return @[@"whitecoaster-1", @"wood", @"red"];
+    return @[@"white", @"wood-1", @"red"];
 }
 
 - (void)previousImage {
-    self.instructionsLabel.hidden = YES;
-    
-    if (!self.imageIndex) {
-        self.imageIndex = 0;
-    }
-    [self updateCoasterImage:[self.images objectAtIndex:self.imageIndex]];
+    self.instructionsContainer.hidden = YES;
     
     if (self.imageIndex == 0) {
         self.imageIndex = [self.images count] - 1;
     } else {
         self.imageIndex--;
     }
+    
+    [self updateCoasterImage:[self.images objectAtIndex:self.imageIndex]];
 }
 
 - (void)nextImage {
-    self.instructionsLabel.hidden = YES;
-    
-    if (!self.imageIndex) {
-        self.imageIndex = 0;
-    }
-
-    [self updateCoasterImage:[self.images objectAtIndex:self.imageIndex]];
+    self.instructionsContainer.hidden = YES;
     
     if (self.imageIndex == [self.images count] - 1) {
         self.imageIndex = 0;
     } else {
         self.imageIndex++;
     }
+    
+    [self updateCoasterImage:[self.images objectAtIndex:self.imageIndex]];
 }
 
 - (void)updateCoasterImage:(NSString *)imageName {
@@ -109,22 +106,59 @@
 
 }
 
-- (void)bannerViewDidLoadAd:(ADBannerView *)banner {
-    if (!self.bannerIsVisible) {
-        if (!self.adBanner.superview) {
-            [self.view addSubview:self.adBanner];
-        }
+- (void)initIadBanner {
+    self.adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, 0, 0)];
+    self.adBanner.delegate = self;
+    self.adBanner.hidden = YES;
+    [self.view addSubview:self.adBanner];
+}
+
+- (void)initGadBanner {
+    self.gadBanner = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait
+                                                    origin:CGPointMake(0, self.view.frame.size.height)];
+
+    self.gadBanner.adUnitID = @"ca-app-pub-5251476832975034/4401427149";
+    self.gadBanner.rootViewController = self;
+    self.gadBanner.delegate = self;
+    self.gadBanner.hidden = YES;
+    [self.view addSubview:self.gadBanner];
+}
+
+- (void)showBanner:(UIView *)banner {
+    if (banner && [banner isHidden]) {
         [UIView beginAnimations:@"animateAdBannerOn" context:NULL];
+        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
         [UIView commitAnimations];
-        self.bannerIsVisible = YES;
+        banner.hidden = NO;
     }
 }
 
+- (void)hideBanner:(UIView *)banner {
+    if (banner && ![banner isHidden]) {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        [UIView commitAnimations];
+        banner.hidden = YES;
+    }
+}
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner {
+    [self hideBanner:self.gadBanner];
+    [self showBanner:self.adBanner];
+}
+
 - (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
-    [self.adBanner removeFromSuperview];
-    [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
-    [UIView commitAnimations];
-    self.bannerIsVisible = NO;
+    [self hideBanner:banner];
+    [self.gadBanner loadRequest:[GADRequest request]];
+}
+
+- (void)adViewDidReceiveAd:(GADBannerView *)bannerView {
+    [self hideBanner:self.adBanner];
+    [self showBanner:self.gadBanner];
+}
+
+- (void)adView:(GADBannerView *)bannerView didFailToReceiveAdWithError:(GADRequestError *)error {
+    [self hideBanner:self.gadBanner];
 }
 
 @end
